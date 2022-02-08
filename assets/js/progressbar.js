fb92d017bf06b6a0dee902a6268c70330e112227
JaCrispy <64677705+JaCrispy4939@users.noreply.github.com>
2022-02-08 10:34:09 -0500

    Update progressbar.js

--- assets/js/progressbar.js
+++ assets/js/progressbar.js
@@ -5,4 +5,5 @@ function myFunction() {
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
+  function.classList.add('is-fixed');
 }

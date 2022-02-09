de36165a8e15e0a47885316a5639a016c1196bf6
JaCrispy <64677705+JaCrispy4939@users.noreply.github.com>
2022-02-09 11:53:00 -0500

    Update progressbar.js

--- assets/js/progressbar.js
+++ assets/js/progressbar.js
@@ -1,7 +1,7 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {scrollprogbar()};
 
-function myFunction() {
+function scrollprogbar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
